e6021d5da80b6125a43581a5f7ae8f79739b4930
3ccapital <44624766+3ccapital@users.noreply.github.com>
2023-11-08 09:27:12 +0000

    Guayavita

--- src/App.js
+++ src/App.js
@@ -1,65 +1,102 @@
-import React, { useState } from 'react'
-import axios from 'axios'
+import React, { useState } from 'react';
 
 function App() {
-  const [data, setData] = useState({})
-  const [location, setLocation] = useState('')
+  const [round, setRound] = useState(0);
+  const [tablePot, setTablePot] = useState(0);
+  const [balance, setBalance] = useState(0);
+  const [minBet, setMinBet] = useState(1);
+  const [firstThrow, setFirstThrow] = useState(0);
+  const [secondThrow, setSecondThrow] = useState(0);
+  const [betAmount, setBetAmount] = useState(0);
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`
+  // Function to handle deposit
+  const handleDeposit = () => {
+    setBalance(balance + minBet);
+  };
 
-  const searchLocation = (event) => {
-    if (event.key === 'Enter') {
-      axios.get(url).then((response) => {
-        setData(response.data)
-        console.log(response.data)
-      })
-      setLocation('')
+  // Function to start a new round
+  const startNewRound = () => {
+    if (round === 0) {
+      setRound(1);
+      setTablePot(minBet);
+      setFirstThrow(0);
+      setSecondThrow(0);
+      setBetAmount(0);
     }
-  }
+  };
+
+  // Function to handle player's turn
+  const handlePlayerTurn = () => {
+    if (round === 0) {
+      alert("You need to start a new round first.");
+    } else {
+      // Simulate dice throws (1 to 6)
+      const firstThrowResult = Math.floor(Math.random() * 6) + 1;
+      const secondThrowResult = Math.floor(Math.random() * 6) + 1;
+      setFirstThrow(firstThrowResult);
+      setSecondThrow(secondThrowResult);
+
+      // Check outcomes based on the dice throws
+      if (firstThrowResult === 1) {
+        alert("You rolled a 1. You lose 1x minimum bet.");
+        setBalance(balance - minBet);
+        setRound(0);
+      } else if (firstThrowResult === 6) {
+        alert("You rolled a 6. You win 1x minimum bet.");
+        setBalance(balance + minBet);
+        setRound(0);
+      } else {
+        // Player has the option to pass or bet
+        alert(`First throw: ${firstThrowResult}\nSecond throw: ${secondThrowResult}`);
+        if (secondThrowResult > firstThrowResult) {
+          alert(`You won the bet of ${betAmount}`);
+          setBalance(balance + betAmount);
+          setRound(0);
+        } else if (secondThrowResult <= firstThrowResult) {
+          alert(`You lost the bet of ${betAmount}`);
+          setBalance(balance - betAmount);
+          setRound(0);
+        }
+      }
+    }
+  };
 
   return (
     <div className="app">
       <div className="search">
-        <div><h4>Weather App - Julian C - ACBI Project</h4></div>
-        <input
-          value={location}
-          onChange={event => setLocation(event.target.value)}
-          onKeyPress={searchLocation}
-          placeholder='Enter Location'
-          type="text" />
+        <div>
+          <h4>Guavita</h4>
+        </div>
       </div>
       <div className="container">
         <div className="top">
-          <div className="location">
-            <p>{data.name}</p>
+          <div>
+            <p>Balance: {balance}</p>
           </div>
-          <div className="temp">
-            {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
+          <div>
+            <p>Minimum Bet: {minBet}</p>
           </div>
-          <div className="description">
-            {data.weather ? <p>{data.weather[0].main}</p> : null}
+          <div>
+            <p>Table Pot: {tablePot}</p>
           </div>
         </div>
 
-        {data.name !== undefined &&
-          <div className="bottom">
-            <div className="feels">
-              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
-              <p>Feels Like</p>
-            </div>
-            <div className="humidity">
-              {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
-              <p>Humidity</p>
-            </div>
-            <div className="wind">
-              {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
-              <p>Wind Speed</p>
-            </div>
+        {/* Button to deposit */}
+        <button onClick={handleDeposit}>Deposit</button>
+
+        {/* Button to start a new round */}
+        <button onClick={startNewRound}>Start New Round</button>
+
+        {/* Button for player's turn */}
+        <button onClick={handlePlayerTurn}>Player's Turn</button>
+
+        {/* Display dice throws */}
+        {round > 0 && (
+          <div>
+            <p>First Throw: {firstThrow}</p>
+            <p>Second Throw: {secondThrow}</p>
           </div>
-        }
-
-
-
+        )}
       </div>
     </div>
   );
